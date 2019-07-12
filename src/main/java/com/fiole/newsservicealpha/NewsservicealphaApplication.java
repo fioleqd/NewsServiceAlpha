198c355c4bb587ad15f13ef2fae308ea636c6b8d
fioleqd <wsq_qd@qq.com>
2019-07-12 20:57:56 +0800

    init

---
+++ src/main/java/com/fiole/newsservicealpha/NewsservicealphaApplication.java
@@ -0,0 +1,13 @@
+package com.fiole.newsservicealpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsservicealphaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsservicealphaApplication.class, args);
+    }
+
+}
